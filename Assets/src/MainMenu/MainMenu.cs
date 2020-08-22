b12ae78e5c694eef3d95fbd8a7460d9460e3350b
Amit Neeman <amitneeman01@gmail.com>
2020-08-22 20:57:29 +0300

    finished main menu, still needs design

---
+++ Assets/src/MainMenu/MainMenu.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+
+public class MainMenu : MonoBehaviour
+{
+    public Button playText;
+    public Button exitText;
+    // Start is called before the first frame update
+    public void Play()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
