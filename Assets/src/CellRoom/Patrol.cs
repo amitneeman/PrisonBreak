23bd4c7b1d85fef2f5dc436269e0d033b553a193
nirchetrit <54546127+nirchetrit@users.noreply.github.com>
2020-08-23 17:44:29 +0300

    added drone in the cell room
    he is doing patrol, if touches the player, he will respawn at the beggining

---
+++ Assets/src/CellRoom/Patrol.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Patrol : MonoBehaviour
+{
+    public float speed;
+    public Transform[] moveSpots;
+    private int randomSpot;
+    private float waitTime;
+    public float startWaitTime;
+    // Start is called before the first frame update
+    void Start()
+    {
+        randomSpot = Random.Range(0,moveSpots.Length);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, moveSpots[randomSpot].position, speed * Time.deltaTime);
+        if (Vector2.Distance(transform.position,moveSpots[randomSpot].position)<0.2f) {
+            if (waitTime <= 0)
+            {
+                randomSpot = Random.Range(0, moveSpots.Length);
+                waitTime = startWaitTime;
+            }
+            else {
+                waitTime -= Time.deltaTime;
+            }
+        }
+    }
+    
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player") {
+            Rigidbody rb = collision.gameObject.GetComponent<Rigidbody>();
+            rb.MovePosition(new Vector3((float)-7.7,(float)5.61,(float)-18.1));
+        }
+    }
+    
+}
