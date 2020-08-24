7995fc0279a976c8772216774e72f2dc326aea44
nirchetrit <54546127+nirchetrit@users.noreply.github.com>
2020-08-24 12:03:38 +0300

    added the laser in the cellRoom

---
+++ Assets/src/CellRoom/Laser.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Laser : MonoBehaviour
+{
+
+     void Update()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, Vector3.right,out hit, 100)) {
+            if (hit.collider.gameObject.tag == "Player") {
+                Rigidbody rb = hit.collider.gameObject.GetComponent<Rigidbody>();
+                rb.MovePosition(new Vector3((float)-7.7, (float)5.61, (float)-18.1));
+            }
+            
+        }
+
+    }
+}
